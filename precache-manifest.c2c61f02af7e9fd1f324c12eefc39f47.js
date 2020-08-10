59220266620610813123ec80f041550d369ddab8
Amrit828 <amritgopi06@gmail.com>
2020-08-10 15:42:54 +0530

    Updates

---
+++ precache-manifest.c2c61f02af7e9fd1f324c12eefc39f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4ba7d6a21df9ba421d00230584ea1b",
+    "url": "/My-first-react-app/index.html"
+  },
+  {
+    "revision": "76d28fc490fac84ceb6a",
+    "url": "/My-first-react-app/static/css/main.d74522ad.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/My-first-react-app/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-first-react-app/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d28fc490fac84ceb6a",
+    "url": "/My-first-react-app/static/js/main.3af23d15.chunk.js"
+  },
+  {
+    "revision": "feba4fae2c2e1eaf73e9",
+    "url": "/My-first-react-app/static/js/runtime-main.5ad688ba.js"
+  }
+]);
